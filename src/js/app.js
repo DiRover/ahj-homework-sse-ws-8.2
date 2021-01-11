4ef5b04cdbfe5cd840d9d43b989933ec335a9b9d
Dmitriy Rovenskiy <arhitec@bk.ru>
2021-01-11 20:56:48 +0300

    code v2.2

--- src/js/app.js
+++ src/js/app.js
@@ -2,27 +2,43 @@ import API from './api';
 
 const container = document.querySelector('[data-id="container"]');
 const modal = document.querySelector('.modal');
-console.log(modal);
+const modalButton = document.querySelector('.form_button');
+const inputForm = document.querySelector('.form_input');
+const addForm = document.querySelector('.add_form');
+const url = 'ws://localhost:7070/ws';
+const api = new API(url);
+const ws = api.getWebSocket();
 
+modal.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //console.log(inputForm.value);
+  const msg = inputForm.value;
+  addForm.reset();
+  if (ws.readyState === WebSocket.OPEN) {
+    ws.send(msg);
+    } else {
+    // Reconnect
+    }
+  
+});
 
 
 
 console.log('ok');
 
-const url = 'ws://localhost:7070/ws';
-const api = new API(url);
 
-const ws = api.getWebSocket();
 
+/*
 ws.addEventListener('open', () => {
   console.log('connected');
   // After this we can send messages
   ws.send('hello!');
 });
-
+*/
 ws.addEventListener('message', (evt) => {
   // handle evt.data
   console.log(evt.data);
+  
 });
 
 ws.addEventListener('close', (evt) => {
@@ -33,3 +49,4 @@ ws.addEventListener('close', (evt) => {
 ws.addEventListener('error', () => {
   console.log('error');
 });
+
