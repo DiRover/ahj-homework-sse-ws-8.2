bceed741dd0399f3c92a7b89166fa68370974dc7
Dmitriy Rovenskiy <arhitec@bk.ru>
2021-01-17 18:43:30 +0300

    code v3.0

--- src/js/api.js
+++ src/js/api.js
@@ -5,25 +5,28 @@
 export default class API {
   constructor(url) {
     this.url = url;
+    this.serverResponse;
+    this.type;
   }
 
   getWebSocket() {
     return new WebSocket(this.url);
   }
-/*
-  load(evt) { // метод для загрузки сообщений
-    const data = JSON.parse(evt.data).field[0]; // получаем данные из ответа сервера
-    const elem = document.createElement('div');
-    elem.setAttribute('class', 'msg');
-    if (data.type === 'freekick') {
-      elem.innerHTML = `<span>&#10071 &#10071</span><span>${data.msg}</span>`;
-    } else if (data.type === 'goal') {
-      elem.innerHTML = `<span>&#9917</span><span>${data.msg}</span>`;
-    } else {
-      elem.innerHTML = `<span></span><p>${data.msg}</p>`;
+
+  responseHandler(e) {
+    //console.log(e.data);
+    const response = JSON.parse(e.data);
+    this.type = response.type;
+    //console.log(type);
+    if (this.type === 'error') {
+      this.serverResponse = response.text;
+      console.log(this.serverResponse);
+    } else if (this.type === 'users list') {
+      this.serverResponse = response.users;
+      console.log(this.serverResponse);
+    } else if (this.type === 'user added') {
+      this.serverResponse = response.users;
+      console.log(this.serverResponse);
     }
-    container.scrollBy(0, 9999999); // прокручиваем страницу до последнего сообщения
-    container.appendChild(elem);
   }
-*/
 }
--- src/js/app.js
+++ src/js/app.js
@@ -5,46 +5,82 @@ const modal = document.querySelector('.modal');
 const modalButton = document.querySelector('.form_button');
 const inputForm = document.querySelector('.form_input');
 const addForm = document.querySelector('.add_form');
+const usersList = document.querySelector('.list');
+const chatForm = document.querySelector('.chat_form');
+const inputChat = document.querySelector('.chat_input');
 const url = 'ws://localhost:7070/ws';
 const api = new API(url);
 const ws = api.getWebSocket();
+let serverResponse;
+/*
+window.addEventListener('load', () => {
+  console.log('page loaded');
+  const msg = { type: 'get users' };
+  const data = JSON.stringify(msg);
+  if (ws.readyState === WebSocket.OPEN) {
+    ws.send(data);
+  } else {
+    // Reconnect
+  }
+})
+*/
 
 modal.addEventListener('submit', (e) => {
   e.preventDefault();
   //console.log(inputForm.value);
-  const msg = { type: 'nickname', nickname: inputForm.value };
-  const data = JSON.stringify(msg)
-  console.log(msg)
+  const msg = { type: 'add user', nickname: inputForm.value };
+  const data = JSON.stringify(msg);
   addForm.reset();
   if (ws.readyState === WebSocket.OPEN) {
     ws.send(data);
     } else {
     // Reconnect
-    }
+  }
   
 });
 
-
+inputChat.addEventListener('change', (e)=> {
+  //e.preventDefault();
+  console.log(e);
+})
 
 console.log('ok');
 
 
 
-/*
 ws.addEventListener('open', () => {
   console.log('connected');
   // After this we can send messages
-  ws.send('hello!');
-});
-*/
-ws.addEventListener('message', (evt) => {
-  // handle evt.data
-  console.log(evt.data);
-  
+  const msg = { type: 'get users' };
+  const data = JSON.stringify(msg);
+  ws.send(data);
 });
 
-ws.addEventListener('close', (evt) => {
-  console.log('connection closed', evt);
+ws.addEventListener('message', (e) => {
+  // handle evt.data
+  api.responseHandler(e);
+  if (api.type === 'user added') {
+    console.log(api.serverResponse);
+    modal.classList.add('hidden');
+    usersList.innerHTML='';
+    api.serverResponse.forEach((user) => {
+      const el = document.createElement('tr');
+      el.innerHTML = `<td>${user}</td>`;
+      usersList.append(el);
+    })
+  } else if (api.type === 'users list') {
+    console.log(api.serverResponse);
+    usersList.innerHTML='';
+    api.serverResponse.forEach((user) => {
+      const el = document.createElement('li');
+      el.innerHTML = `${user}`;
+      usersList.append(el);
+    })
+  }
+});
+
+ws.addEventListener('close', (e) => {
+  console.log('connection closed', e);
   // After this we can't send messages
 });
 
